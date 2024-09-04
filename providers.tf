fef0dde3897c261d98e53ced14d19bf57e1d45b9
kumashan1102 <kshantanu1@gmail.com>
2024-09-04 23:28:47 +0530

    backend file

--- providers.tf
+++ providers.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      #version = "5.61.0"
-      version = var.tfversion
+      version = "5.62.0"
+      #version = var.tfversion
     }
   }
 }
